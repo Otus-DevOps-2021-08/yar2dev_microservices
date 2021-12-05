863f6dc89593a1b0f26f1c776420fc9785305757
Yaroslav Ermilov <yarkiy.dev@gmail.com>
2021-12-05 15:38:18 +0300

    test tag

---
+++ gitlab-ci/ansible/inventory_yc.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+#This script get addresses from yandex cloud list and generate inventory json output for ansible
+import os
+import sys
+import argparse
+from subprocess import Popen, PIPE
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+class AnsbleInventory(object):
+
+    def __init__(self):
+        self.inventory = {}
+        self.read_cli_args()
+
+
+        if self.args.list:
+            self.inventory = self.ansible_inventory()
+
+        print (json.dumps(self.inventory));
+
+    # Template for inventory output
+    def ansible_inventory(self):
+
+
+        return {
+
+            'gitlab': {
+
+
+
+                'hosts': gitlab_ext_address,
+                'vars': {
+                    'example': 'value',
+                }
+            },
+
+
+
+            '_meta': {
+                'hostvars': {}
+            }
+        }
+
+    def read_cli_args(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--list', action = 'store_true')
+        parser.add_argument('--host', action = 'store')
+        self.args = parser.parse_args()
+
+
+# Get app and db servers addresses from yc list.
+
+yc_command = "(yc compute instance list)"
+
+gitlab_ext_address = []
+
+
+skip = False
+response =Popen(yc_command, stdout=PIPE, stderr=PIPE,universal_newlines=True, shell=True)
+
+with Popen(yc_command, stdout=PIPE, stderr=PIPE,universal_newlines=True, shell=True) as process:
+
+    for line in process.stderr:
+        skip = True
+
+#Parsing yc output
+    for line in process.stdout:
+
+
+
+        if "gitlab" in line:
+
+
+            line = line.split("|")
+            addr = line[5].strip()
+
+            gitlab_ext_address.append(addr)
+
+
+
+
+
+
+if skip != True:
+    AnsbleInventory()
---
+++ gitlab-ci/terraform/main.tf
@@ -0,0 +1,64 @@
+provider "yandex" {
+
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+data "yandex_compute_image" "image" {
+  family = "ubuntu-1804-lts"
+}
+resource "yandex_compute_instance" "gitlab" {
+
+  name = "${var.instance_name}"
+
+  zone = var.zone_app
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+
+  }
+
+
+  resources {
+    cores  = 4
+    memory = 4
+
+  }
+
+
+  boot_disk {
+    initialize_params {
+      size     = 33
+      type     = "network-ssd"
+      image_id = data.yandex_compute_image.image.id
+    }
+  }
+
+  network_interface {
+
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+#waiting for machine start
+  provisioner "remote-exec" {
+    inline = [
+    "echo"]
+  }
+
+
+  connection {
+    type  = "ssh"
+    host  = self.network_interface[0].nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    private_key = file(var.private_key_path)
+  }
+
+
+  provisioner "local-exec" {
+    command     = "ansible-playbook playbooks/site.yml"
+    working_dir = "../ansible"
+  }
+
+}
